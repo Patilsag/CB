8991ac75e37b2c568b6e3618a217b81249fd478f
Sagar Patil <159707425+Patilsag@users.noreply.github.com>
2025-04-30 15:07:26 +0200

    Update localama.py

--- localama.py
+++ localama.py
@@ -1,10 +1,10 @@
-from langchain_openai import ChatOpenAI
+from langchain_community.llms import Ollama
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from langchain_community.llms import ollama
 import streamlit as st
 import os
 from dotenv import load_dotenv
+
 
 # Load environment variables
 load_dotenv()
